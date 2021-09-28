f55229084d194a9c75565ae9731fe1c4e307714e
Mimi <1119186082@qq.com>
2021-09-28 21:36:43 +0800

    Install @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
     input: 'src/index.js',
-    plugins: [resolve()]
+    output: {
+        format: 'iife'
+    },
+    plugins: [resolve(), commonjs()]
 };
